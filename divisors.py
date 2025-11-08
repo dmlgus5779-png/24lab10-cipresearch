ec16889bb8df5e8e3a8c7f022f013118292c3511
dmlgus5779-png <dmlgus5779@gachon.ac.kr>
2025-11-09 02:56:27 +0900

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,7 +1,8 @@
 import sys
-number = int(sys.argv[1])
+number=int(sys.argv[1])
+for i in range(1, 100 + 1):
+    if 100 % i == 0:
+        print(i, end=" ")
 
-for i in range(1, number+1):
-  if number % i == 0:
-    print(i, end=" ")
+print() 
 
